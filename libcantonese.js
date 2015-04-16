28b1031df0522b9dfa9a25fce62636f46dfc4d6b
Hill Ma <mahiuchun@users.noreply.github.com>
2015-04-15 20:51:37 -0700

    new way of parsing and conversion; wip

--- libcantonese.js
+++ libcantonese.js
@@ -5,147 +5,135 @@
   } else {
     this.libcantonese = libcantonese;
   }
-  var jyutping_onsets = [
-    'ng', 'gw', 'kw',
-    'b', 'p', 'm', 'f',
-    'd', 't', 'n', 'l',
-    'g', 'k', 'h', 'z',
-    'c', 's', 'j', 'w'
+  var LSHK = 0;
+  var YALE = 1;
+  var HKED = 2;
+  var SLAU = 3;
+  var onsets = [
+    ['b','p','m','f','d','t','n','l','g','k','h','ng','z' ,'c', 's','gw','kw','j','w'], // lshk
+    ['b','p','m','f','d','t','n','l','g','k','h','ng','j' ,'ch','s','gw','kw','y','w'], // yale
+    ['b','p','m','f','d','t','n','l','g','k','h','ng','dz','ts','s','gw','kw','j','w'], // hked
+    ['b','p','m','f','d','t','n','l','g','k','h','ng','j' ,'ch','s','gw','kw','y','w']  // slau
   ];
-  var jyutping_to_yale_onset = {
-    'z': 'j',
-    'c': 'ch',
-    'j': 'y'
-  };
-  var jyutping_to_yale_final = {
-    'aa': 'a',
-    'oe': 'eu',
-    'oeng': 'eung',
-    'oek': 'euk',
-    'eoi': 'eui',
-    'eon': 'eun',
-    'eot': 'eut'
-  };
-  var jyutping_to_hked_onset = {
-    'z': 'dz',
-    'c': 'ts'
-  };
-  var jyutping_to_hked_final = {
-    'aa': 'a',
-    'eoi': 'oey',
-    'eon': 'oen',
-    'eot': 'oet',
-    'yu': 'y',
-    'yun': 'yn',
-    'yut': 'yt'
-  };
-  var jyutping_to_sidneylau_onset = {
-    'z': 'j',
-    'c': 'ch',
-    'j': 'y'
-  };
-  var jyutping_to_sidneylau_final = {
-    'aa': 'a',
-    'o': 'oh',
-    'ou': 'o',
-    'u': 'oo',
-    'ui': 'ooi',
-    'un': 'oon',
-    'ut': 'oot',
-    'oe': 'euh',
-    'oeng': 'eung',
-    'oek': 'euk',
-    'eoi': 'ui',
-    'eon': 'un',
-    'eot': 'ut',
-    'yu': 'ue',
-    'yun': 'uen',
-    'yut': 'uet'
-  };
-  var jyutping_to_sidneylau_tones = {
-    '1': '<sup>1°</sup>',
-    '2': '<sup>2</sup>',
-    '3': '<sup>3</sup>',
-    '4': '<sup>4</sup>',
-    '5': '<sup>5</sup>',
-    '6': '<sup>6</sup>',
-    '': ''
-  }
-  var parse_lshk = function (jp) {
+  var finals = [
+    ['aa','aai','aau','aam','aan','aang','aap','aat','aak','ai','au','am','an','ang','ap','at','ak',
+    'e','ei','eng','ek','i','iu','im','in','ing','ip','it','ik','o' ,'oi','ou','on','ong','ot','ok',
+    'u' ,'ui' ,'un' ,'ung','ut' ,'uk','oe' ,'oeng','oek','eoi','eon','eot','yu','yun','yut','m','ng'], //lshk
+    ['a' ,'aai','aau','aam','aan','aang','aap','aat','aak','ai','au','am','an','ang','ap','at','ak',
+    'e','ei','eng','ek','i','iu','im','in','ing','ip','it','ik','o' ,'oi','ou','on','ong','ot','ok',
+    'u' ,'ui' ,'un' ,'ung','ut' ,'uk','eu' ,'eung','euk','eui','eun','eut','yu','yun','yut','m','ng'], //yale
+    ['a' ,'aai','aau','aam','aan','aang','aap','aat','aak','ai','au','am','an','ang','ap','at','ak',
+    'e','ei','eng','ek','i','iu','im','in','ing','ip','it','ik','o' ,'oi','ou','on','ong','ot','ok',
+    'u' ,'ui' ,'un' ,'ung','ut' ,'uk','oe' ,'oeng','oek','oey','oen','oet','y' ,'yn' ,'yt' ,'m','ng'], //hked
+    ['a' ,'aai','aau','aam','aan','aang','aap','aat','aak','ai','au','am','an','ang','ap','at','ak',
+    'e','ei','eng','ek','i','iu','im','in','ing','ip','it','ik','oh','oi','o' ,'on','ong','ot','ok',
+    'oo','ooi','oon','ung','oot','uk','euh','eung','euk','ui', 'un' ,'ut' ,'ue','uen','uet','m','ng']  //slau
+  ];
+  function parse_generic (s, index) {
     var parts = ['', '', ''];
     var offset = 0;
-    // find onset and onset offset
-    for (var i = 0; i < jyutping_onsets.length; i++) {
-      if (jp.indexOf(jyutping_onsets[i]) === 0) {
-        parts[0] = jyutping_onsets[i];
-        offset = jyutping_onsets[i].length;
-        break;
+    // find onset
+    var max = 0;
+    var maxid = -1;
+    for (var i = 0; i < onsets[index].length; i++) {
+      if (s.indexOf(onsets[index][i]) === 0 && onsets[index][i].length > max) {
+        max = onsets[index][i].length;
+        maxid = i;
       }
     }
-    var final_len = jp.length - offset;
-    // if last character is valid tone, take it
-    if (/^[1-6]$/.test(jp[jp.length-1])) {
-      parts[2] = jp[jp.length-1];
-      final_len -= 1;
+    if (maxid >= 0) {
+      parts[0] = onsets[index][maxid];
+      offset += onsets[index][maxid].length;
     }
-    // FIXME: check whether final is valid
-    parts[1] = jp.substr(offset, final_len);
+    // find final
+    max = 0;
+    maxid = -1;
+    for (var i = 0; i < finals[index].length; i++) {
+      if (s.indexOf(finals[index][i]) === offset && finals[index][i].length > max) {
+        max = finals[index][i].length;
+        maxid = i;
+      }
+    }
+    if (maxid >= 0) {
+      parts[1] = finals[index][maxid];
+      offset += finals[index][maxid].length;
+    }
+    // find tone
+    if (s.length > offset && /^[0-9]$/.test(s[offset])) {
+      parts[2] = s[offset];
+    }
+    return parts;
+  }
+  var parse_lshk = function (s) {
+    var parts = parse_generic(s, LSHK);
+    if (!(/^[1-6]$/.test(parts[2]))) {
+      parts[2] = '';
+    }
     return parts;
   };
   libcantonese.parse_lshk = parse_lshk;
-  libcantonese.lshk_to_yale = function (jp) {
-    var parts = parse_lshk(jp);
-    // map onset
-    if (parts[0] in jyutping_to_yale_onset) {
-      parts[0] = jyutping_to_yale_onset[parts[0]];
+  var parse_yale = function (s) {
+    var parts = parse_generic(s, YALE);
+    if (!(/^[1-6]$/.test(parts[2]))) {
+      parts[2] = '';
     }
-    // map final
-    if (parts[1] in jyutping_to_yale_final) {
-      parts[1] = jyutping_to_yale_final[parts[1]];
+    return parts;
+  };
+  libcantonese.parse_yale = parse_yale;
+  var parse_hked = function (s) {
+    var parts = parse_generic(s, HKED);
+    if (!(/^[1-9]$/.test(parts[2]))) {
+      parts[2] = '';
     }
-    // fix double y
-    if (parts[0] === 'y' && parts[1].indexOf('y') === 0) {
-      parts[0] = '';
+    return parts;
+  };
+  libcantonese.parse_hked = parse_hked;
+  var parse_slau = function (s) {
+    var parts = parse_generic(s, SLAU);
+    if (!(/^[1-6]$/.test(parts[2]))) {
+      parts[2] = '';
     }
-    // TODO: yale tone mark
-    return parts.join('');
-  }
-  libcantonese.lshk_to_hked = function (jp) {
-    var parts = parse_lshk(jp);
-    // map onset
-    if (parts[0] in jyutping_to_hked_onset) {
-      parts[0] = jyutping_to_hked_onset[parts[0]];
-    }
-    // map final
-    if (parts[1] in jyutping_to_hked_final) {
-      parts[1] = jyutping_to_hked_final[parts[1]];
-    }
-    var tail = parts[1][parts[1].length-1];
-    // fix hked tone
-    if (tail === 'k' || tail === 'p' || tail === 't') {
-      switch (parts[2]) {
-      case '1':
-        parts[2] = '7';
-        break;
-      case '3':
-        parts[2] = '8';
-        break;
-      case '6':
-        parts[2] = '9';
+    return parts;
+  };
+  libcantonese.parse_slau = parse_slau;
+  function convert_generic(s, fidx, tidx) {
+    var parts = parse_generic(s, fidx);
+    for (var i = 0; i < onsets[fidx].length; i++) {
+      if (parts[0] === onsets[fidx][i]) {
+        parts[0] = onsets[tidx][i];
         break;
       }
     }
-    return parts.join('');
+    for (var i = 0; i < finals[fidx].length; i++) {
+      if (parts[1] === finals[fidx][i]) {
+        parts[1] = finals[tidx][i];
+        break;
+      }
+    }
+    return parts;
   }
-  libcantonese.lshk_to_slau = function (jp) {
-    var parts = parse_lshk(jp);
-    if (parts[0] in jyutping_to_sidneylau_onset) {
-      parts[0] = jyutping_to_sidneylau_onset[parts[0]];
-    }
-    if (parts[1] in jyutping_to_sidneylau_final) {
-      parts[1] = jyutping_to_sidneylau_final[parts[1]];
-    }
-    parts[2] = jyutping_to_sidneylau_tones[parts[2]];
+  libcantonese.lshk_to_yale = function (s) {
+    var parts = convert_generic(s, LSHK, YALE);
+    return parts.join('');
+  };
+  libcantonese.yale_to_lshk = function (s) {
+    var parts = convert_generic(s, YALE, LSHK);
+    return parts.join('');
+  };
+  libcantonese.lshk_to_hked = function (s) {
+    var parts = convert_generic(s, LSHK, HKED);
+    return parts.join('');
+  };
+  libcantonese.hked_to_lshk = function (s) {
+    var parts = convert_generic(s, HKED, LSHK);
+    return parts.join('');
+  };
+  libcantonese.lshk_to_slau = function (s) {
+    var parts = convert_generic(s, LSHK, SLAU);
+    return parts.join('');
+  };
+  libcantonese.slau_to_lshk = function (s) {
+    var parts = convert_generic(s, SLAU, LSHK);
     return parts.join('');
   }
 }.call(this));
